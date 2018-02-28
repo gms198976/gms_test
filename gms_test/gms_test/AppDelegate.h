3752017e7df0616b5a4ef8bed38bc2949a341485
wb_gumingsheng <wb_gumingsheng@zhongan.io>
2018-02-28 15:30:16 +0800

    初次提交

---
+++ gms_test/gms_test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gms_test
+//
+//  Created by gumingsheng on 2018/2/28.
+//  Copyright © 2018年 GMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gms_test/gms_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gms_test
+//
+//  Created by gumingsheng on 2018/2/28.
+//  Copyright © 2018年 GMS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
